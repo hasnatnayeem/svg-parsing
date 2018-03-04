95df3ee613cb3dea7c67f22b4612cf62078476ff
Abul Hasnat Nayeem <sakil58@gmail.com>
2018-03-05 00:59:14 +0600

    Recieve file name via GET request

--- shell.php
+++ shell.php
@@ -1,5 +1,7 @@
 <?php
-    $file_name = "test.svg";
+    $directory = "files/";
+    $file_name = $directory.$_GET['f'];
+
     $command = "python svg_to_json.py " . $file_name;
     $output = shell_exec($command);
     echo "<pre>$output</pre>";
